a67573d41aa449900b76f20f5c723ed711758096
Brayan Marin Guirales <brayan6590@gmail.com>
2025-08-26 16:16:57 -0500

    feat: adding two more test cases

--- modulo1/tests/example.spec.js
+++ modulo1/tests/example.spec.js
@@ -1,7 +1,34 @@
 const { test, expect } = require("@playwright/test");
 
-test("Nuestro primer test", async ({ page }) => {
+test("Primer Test: Nuestro primer test", async ({ page }) => {
   await page.goto("https://example.com");
   const tittle = await page.title();
   expect(tittle).toBe("Example Domain");
+});
+
+test("Segundo Test: Probando que el h1 de example.com tenga algún contenido", async ({
+  page,
+}) => {
+  await page.goto("https://example.com");
+  const h1_text = await page.textContent("h1");
+  expect(h1_text).not.toBe("");
+});
+
+// Test cases usando la página web https://playwright.dev/
+test("Tercer Test: Verificando el título de la página de Playwright", async ({
+  page,
+}) => {
+  await page.goto("https://playwright.dev/");
+  //Título contenga la palabra Playwright
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("Cuarto Test: obtener el link 'Get Started'", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  // Hacemos click en el link 'Get Started'
+  await page.getByRole("link", { name: "Get started" }).click();
+  // Buscamos que la página en cuestión tenga el encabezado 'Installation'
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
 });
